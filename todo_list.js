ddd2a30372f4239bb61d73f86498f7cf084edd67
Brian Kennedy & James Newman <pair+briankennedy1.jnewman12@devbootcamp.com>
2015-06-15 15:57:28 -0700

    OOJS tasks completed

--- todo_list.js
+++ todo_list.js
@@ -1,10 +1,49 @@
-var newTodoList = function() {
-	// ???
+/*
+pseudocode
+- add
+- remove
+- list / tasks
+-
+
+*/
+
+var TodoList = function() {
+  this.list = [];
 };
 
+TodoList.prototype.add = function(item) {
+  this.list.push(item);
+}
+
+TodoList.prototype.remove = function(item) {
+  this.list.splice(item, 1);
+}
+
+TodoList.prototype.tasks = function() {
+  return this.list;
+}
+
+//Task {id: 1, description: 'bread', completed: false}
+var Task = function(id, description) {
+  this.id = id;
+  this.description = description;
+  this.completed = false;
+}
+
+Task.prototype.complete = function() {
+  this.completed = true;
+}
+
+breadTask = new Task(1, "bread making")
+milkTask = new Task(2, "milk making")
+cheeseTask = new Task(3, "cheese making")
 
 
 // Driver code
 
 
-var todoList = newTodoList();
+var todoList = new TodoList();
+todoList.add(breadTask)
+todoList.add(milkTask)
+todoList.add(cheeseTask)
+
