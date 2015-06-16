c09f954d978e8bfcd2916e83ed6e2c0da5ff5b9b
Patrick S. Betti <patreek3@gmail.com>
2015-06-15 19:51:28 -0700

    add remove, list etc methods all working but function is getting called 9 times instead of 3 for some reason tbdebugged

--- todo_list.js
+++ todo_list.js
@@ -1,10 +1,83 @@
-var newTodoList = function() {
-	// ???
+
+
+var Item = function(description, id) {
+  this.description = description;
+  this.id = id
+  this.completed = false;
 };
 
+Item.prototype.complete = function() {
+    this.completed = true;
+  };
 
+TodoList.prototype.generate_id = function() {
+  return (this.tasks.length + 1)
+};
 
+function TodoList() {
+	this.tasks = []
+
+  TodoList.prototype.add = function(item) {
+    this.tasks.push(new Item(item, this.generate_id()))
+  };
+
+  TodoList.prototype.remove = function(index) {
+    this.tasks.slice(index, 1)
+  };
+
+  TodoList.prototype.list = function() {
+     for (var i = 0; i < this.tasks.length; i++) {
+      console.log(this.tasks[i].id + ". " + this.tasks[i].description + ": " + this.tasks[i].completed);
+    };
+  };
+
+  TodoList.prototype.complete = function(index) {
+    this.tasks[index].completed = true
+  };
+};
 // Driver code
 
 
-var todoList = newTodoList();
+
+
+// Note we are using a JavaScript constructor now.
+var groceryList = new TodoList();
+groceryList.add('bread');
+groceryList.add('cheese');
+groceryList.add('milk');
+
+
+// tasks is now an array of Task objects
+groceryList.tasks //-> [Task, Task, Task]
+
+groceryList.list();
+//> Task {id: 1, description: 'bread', completed: false}
+//> Task {id: 2, description: 'cheese', completed: false}
+//> Task {id: 3, description: 'milk', completed: false}
+
+
+// getting a task object
+var breadTask = groceryList.tasks[0];
+
+breadTask.id //-> 1 (some unique numerical ID)
+breadTask.description //-> 'bread'
+breadTask.completed //-> false
+
+
+// This should complete the task
+breadTask.complete();
+
+breadTask.completed //-> true
+
+groceryList.list();
+//> Task {id: 1, description: 'bread', completed: true}
+//> Task {id: 2, description: 'cheese', completed: false}
+//> Task {id: 3, description: 'milk', completed: false}
+
+
+// This should remove the task from the todo list
+groceryList.remove(breadTask);
+
+groceryList.list();
+//> Task {id: 2, description: 'cheese', completed: false}
+//> Task {id: 3, description: 'milk', completed: false}
